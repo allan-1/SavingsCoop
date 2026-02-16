db8f6db9a37c140090ce0eaccce3a2380fa3c10b
Allan <amuturi01@gmail.com>
2026-02-16 12:56:17 +0300

    Initial Commit

---
+++ SavingsCoop/SavingsCoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavingsCoopApp.swift
+//  SavingsCoop
+//
+//  Created by Allan Maina on 16/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct SavingsCoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
